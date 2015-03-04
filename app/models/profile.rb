87feee9824ef055606832bcdfa4443ceb95f25da
Thomas Midgett <tmidge20@gmail.com>
2015-03-04 14:29:15 -0500

    #masoned

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,13 @@
-class Profile
+class Profile < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  before_create :parse
 
   def initialize(username)
-    @content = HTTParty.get(
-        "https://api.github.com/users/#{username}",
-        :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
-                     "User-Agent" => "anyone"
-                    }
-    )
+    @username = username
+  end
+
+  def self.create_from_username(username)
+
   end
 
   def username
@@ -32,5 +33,24 @@ class Profile
   def number_following
     @content["following"]
   end
+
+  private
+    def parse
+      number_following = body["following"]
+      #...
+    end
+
+    def cache
+      if Profile.where(username: @username).blank?
+        @content = HTTParty.get(
+            "https://api.github.com/users/#{username}",
+            :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+                         "User-Agent" => "anyone"
+                        }
+        )
+      else
+        @content = Profile.find_by(username: @username).body
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/create'
+
   get 'repositories/:username' => 'repositories#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
